52e1fa33870e4df434f7f78b55b042d20ea89cf7
Veysal <tveysal93@mail.ru>
2025-06-22 14:53:56 +0300

    Начинаем создание основного приложения

---
+++ app.py
@@ -0,0 +1,19 @@
+import flet as ft
+
+# Основное приложение
+def main(page: ft.Page):
+    page.title = "User table"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.bgcolor = ft.colors.BLACK
+
+#
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
