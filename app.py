4fccf1b1ff2df89ce404143f9d31ee6a1c7780f5
Veysal <tveysal93@mail.ru>
2025-06-26 09:40:45 +0300

    Добавлены два выпадающимх списка для статуса оплаты и статуса работы с выбором цветом для каждого статуса

--- app.py
+++ app.py
@@ -58,6 +58,66 @@ def main(page: ft.Page):
     )
 
 
+    #Установка цвета статуса работы для выпадающего списка
+    def update_work_status_color(e):
+        if work_status_dropdown.value == "В работе":
+            work_status_dropdown.text_style.color = ft.TextStyle(color=ft.colors.ORANGE)
+        elif work_status_dropdown.value == "Выполнено":
+            work_status_dropdown.text_style.color = ft.TextStyle(color=ft.colors.GREEN)
+        elif work_status_dropdown.value == "Отменено":
+            work_status_dropdown.text_style.color = ft.TextStyle(color=ft.colors.RED)            
+        page.update()
+
+    # Выпадающий список
+    work_status_dropdown = ft.Dropdown(
+        label="Статус работы",
+        width=200,
+        options=[
+            ft.dropdown.Option("В работе"),
+            ft.dropdown.Option("Выполнено"),
+            ft.dropdown.Option("Отменено")
+        ],
+        value = "В работе",
+        on_change=update_work_status_color,
+        text_style=ft.TextStyle(color=ft.colors.YELLOW),
+    )
+
+    #Установка цвета статуса оплаты для выпадающего списка
+    def update_payment_status_color(e):
+        if payment_status_dropdown.value == "Оплачено":
+            payment_status_dropdown.text_style.color = ft.TextStyle(color=ft.colors.GREEN)
+        elif payment_status_dropdown.value == "Не оплачено":
+            payment_status_dropdown.text_style.color = ft.TextStyle(color=ft.colors.RED)
+        elif payment_status_dropdown.value == "Долг":
+            payment_status_dropdown.text_style.color = ft.TextStyle(color=ft.colors.ORANGE)            
+        page.update()
+
+ 
+    # Выпадающий список - статус оплаты
+    payment_status_dropdown = ft.Dropdown(
+        label="Статус оплаты",
+        width=200,
+        options=[
+            ft.dropdown.Option("Оплачено"),
+            ft.dropdown.Option("Не оплачено"),
+            ft.dropdown.Option("Долг"),
+        ],
+        value = "Не оплачено",
+        text_style=ft.TextStyle(color=ft.colors.RED),
+        on_change=update_payment_status_color
+    )
+
+    payment_amount_input = ft.TextField(
+        label="Сумма оплаты",
+        width=200,
+        keyboard_type=ft.KeyboardType.NUMBER,
+        text_style=ft.TextStyle(color=ft.colors.YELLOW),
+        border = ft.InputBorder.OUTLINE,
+        border_color=ft.colors.BLUE,
+        focused_border_color=ft.colors.YELLOW,
+        border_width= 2,
+        border_radius=10
+    )
 
 
 
