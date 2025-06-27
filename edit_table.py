810e7486eefe083e1af082485dc4710d91f10718
Veysal <tveysal93@mail.ru>
2025-06-27 14:06:20 +0300

    Исправлены небольшие недочеты с подключением к базе данных в файле edit_table

--- edit_table.py
+++ edit_table.py
@@ -3,7 +3,7 @@ import flet as ft
 
 # Функция для обновления данных в БД
 def update_order_in_db(order_id, new_date,new_payment_status):
-    conn = sqlite3.connect('orders.db')
+    conn = sqlite3.connect('work_tracker.db')
     cursor = conn.cursor()
     cursor.execute("""
         UPDATE orders
@@ -16,12 +16,9 @@ def update_order_in_db(order_id, new_date,new_payment_status):
 
 # Функция для поиска заказа по ID
 def get_order_by_id(order_id):
-    conn = sqlite3
+    conn = sqlite3.connect('work_tracker.db')
     cursor = conn.cursor()
-    cursor.execute("""
-        SELECT * FROM orders
-        WHERE order_id = ?
-    """, (order_id,))
+    cursor.execute("""SELECT * FROM orders WHERE order_id = ?""", (order_id,))
     order = cursor.fetchone()
     conn.close()
     return order
